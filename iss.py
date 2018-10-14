5cd687a94a63f1fc4c7306ae709c3c88ac1f516f
ao7one <ao7one@gmail.com>
2018-10-14 13:02:36 +0100

    Initial commit

--- iss.py
+++ iss.py
@@ -5,7 +5,7 @@ import turtle
 
 # Set up the parameters we want to pass to the API.
 # This is the latitude and longitude of Horton Heath.
-parameters = {"lat": 50.95, "lon": 1.29}
+parameters = {"lat": 50.9546, "lon": 1.2951}
 
 # Query ISS API
 response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
@@ -13,14 +13,32 @@ response = requests.get("http://api.open-notify.org/iss-pass.json", params=param
 # Printer response
 print(response.status_code)
 
+print("For Alexander")
+print(response.content)
+
 # Get content and overhead time in seconds
 result = json.loads(response.content)
 over = result['response'][1]['risetime']
 
 # Print time in readable format
-print(time.ctime(over))
+ctime = time.ctime(over)
+print(ctime)
 
 # Output nicely
 screen = turtle.Screen()
-screen.setup(720, 360)
-screen.bgpic("world.jpg")
+screen.setup(1440, 720)
+screen.setworldcoordinates(-360,-180,360, 180)
+screen.bgpic("world.png")
+
+location = turtle.Turtle()
+location.penup()
+location.color('red')
+location.goto(1.2951,50.9546)
+location.dot(5)
+
+# Print date
+style = ('Arial', 14, 'bold')
+location.write(ctime, font=style)
+
+
+time.sleep(10)
