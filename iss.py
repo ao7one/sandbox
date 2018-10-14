077635924ad6e625b5dcbbd11e8cb3e1d6422bf8
ao7one <ao7one@gmail.com>
2018-10-14 11:33:49 +0100

    Initial commit

--- iss.py
+++ iss.py
@@ -1,7 +1,19 @@
 import requests
+import json
+import time
+
+# Set up the parameters we want to pass to the API.
+# This is the latitude and longitude of Horton Heath.
+parameters = {"lat": 50.95, "lon": 1.29}
 
 # Query ISS API
-response = requests.get("http://api.open-notify.org/iss-now.json")
+response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
 
 # Printer response
 print(response.status_code)
+
+result = json.loads(response.content)
+over = result['response'][1]['risetime']
+
+print(time.ctime(over))
+
